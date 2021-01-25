a69a53b74551efaebf366b0e9e668318e3941d6e
marcoalvesalmeida <marcoalvesneto@gmail.com>
2021-01-25 16:53:29 -0300

    Código Final Primeira Aula

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Widget from '../src/components/Widget';
+import { QuizContainer } from '../styles/pages/home';
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+
+import db from '../db.json';
+
+export default function Home() {
+  return ( 
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/marcoalvesalmeida" />
+    </QuizBackground>
+  );
+}
